0e801f7d7cf604d8b3412d27526909c5394e996e
lixu12 <lixu12@58.com>
2021-04-12 20:02:47 +0800

    修改ui

--- lib/leak_detail_page.dart
+++ lib/leak_detail_page.dart
@@ -2,7 +2,6 @@
 
 
 import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -72,53 +71,72 @@ class _LeakDetailState extends State<LeakDetailPage>{
       child: ListView.builder(
         itemBuilder: (context,index){
           final curObj = retainList[index];
-          return buildItem(context,curObj);
+          return buildItem(context,curObj,index);
         },
         itemCount: retainList.length,
       ),
     );
   }
 
-  Widget buildItem(BuildContext context,RetainInfo retainInfo){
+  Widget buildItem(BuildContext context,RetainInfo retainInfo,int index){
     final className = retainInfo.className;
     final filedName = retainInfo.field;
     return GestureDetector(
       child: Container(
-      height: 70,
-      child: Row(
-        children: <Widget>[
-          Container(
-            height: 70,
-            width: 20,
-            child: leftItem(),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-          ),
-          Expanded(child: rightItem(className,filedName))
-        ],
-      ),
-    )
+        height: 80,
+        child: Row(
+          children: <Widget>[
+            Container(
+              width: 40,
+              child: leftItem(index),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+            ),
+            Expanded(child: rightItem(className,filedName))
+          ],
+        ),
+      )
     );
   }
 
-  Widget leftItem() {
+  Widget leftItem(int index) {
     return Stack(
       children: <Widget>[
         Padding(
-          padding: EdgeInsets.only(left: 1),
+          padding: EdgeInsets.only(left: 15),
           child: VerticalDivider(
-            thickness: 2,
+            thickness: 1,
+            color: Color(0xFFFF552E),
           ),
         ),
         Padding(
-          padding: EdgeInsets.only(left: 5, top: 25),
+          padding: EdgeInsets.only(left: 5, top: 0),
           child: Container(
-            width: 10,
-            height: 10,
+            width: 35,
+            height: 35,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              color: Color(0xFFFF552E),
+              color: Colors.white,
+            ),
+            child: Center(
+              child: Container(
+                width: 25,
+                height: 25,
+                child: Center(
+                    child: Text(
+                      index.toString(),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10
+                      ),
+                    )
+                ),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFFFF552E),
+                ),
+              ),
             ),
           ),
         ),
@@ -128,15 +146,10 @@ class _LeakDetailState extends State<LeakDetailPage>{
 
   Widget rightItem( String className,String fieldName) {
     return Container(
-           height: 50,
-           decoration: new BoxDecoration(
-              color: Colors.white,
-              //设置四周圆角 角度 这里的角度应该为 父Container height 的一半
-              borderRadius: BorderRadius.all(Radius.circular(25.0)),
-              //设置四周边框
-              border: new Border.all(width: 1, color:Color(0xFFFF552E)),
-          ),
-           child: Center(child: Text(className+'.'+fieldName,style: TextStyle(color: Colors.black)))
+           height: 80,
+           child: Text(
+              'ClassName:  ' +className+'\n'+'FieldName :  ' +fieldName,
+               style: TextStyle(color:  Color(0xFFFF552E)))
           );
 
   }
